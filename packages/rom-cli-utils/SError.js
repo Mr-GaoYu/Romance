cf66203d021a2f28222e3f0638d706c5dbc8f21e
August <August.Gao@mspbots.ai>
2022-04-19 17:36:55 +0800

    2

---
+++ packages/rom-cli-utils/SError.js
@@ -0,0 +1,13 @@
+module.exports = class SError extends Error {
+  constructor(msg, type, stack) {
+      super(msg || 'San error');
+      this.name = type || 'SError';
+      if (stack && typeof stack === 'string' || Array.isArray(stack)) {
+          this.stack = Array.isArray(stack) ? stack.join('\n') : stack;
+      }
+      else {
+          Error.captureStackTrace(this, SError);
+      }
+
+  }
+};
