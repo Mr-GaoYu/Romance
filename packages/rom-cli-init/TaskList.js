5f6ced98d09599f2ae8f50d85d68eb07f5685464
August.Gao <August.Gao@mspbots.ai>
2022-07-04 23:09:45 +0800

    1

--- packages/rom-cli-init/TaskList.js
+++ packages/rom-cli-init/TaskList.js
@@ -1,37 +1,60 @@
-const { option } = require("yargs");
+const {
+  option
+} = require("yargs");
 
 class Task {
-    constructor(taskListInstance, taskFn) {
-        this.taskListInstance = taskListInstance;
-        this.status = '';
-        this.taskFn = taskFn;
+  constructor(taskListInstance, taskFn) {
+    this.taskListInstance = taskListInstance;
+    this.status = '';
+    this.taskFn = taskFn;
+  }
+  getContext() {
+    return this.taskListInstance.getContext();
+  }
+  run() {
+    this.status = 'running';
+    this.taskFn(this.getContext(), this);
+  }
+  skip(skipReason) {
+    this.status = 'skiped';
+    this.taskListInstance.next(skipReason);
+  }
+  complete() {
+    if (this.status === 'running') {
+      this.status = 'done';
+      this.taskListInstance.next();
     }
-
-    getContext() {
-        return this.taskListInstance.getContext();
+  }
+  error(err) {
+    if (this.status === 'running') {
+      this.status = 'failed';
+      this.taskListInstance.fail(err);
     }
-
-    run() {
-        this.status = 'running';
-        this.taskFn(this.getContext(), this)
+  }
+  info(data) {
+    if (data) {
+      this.taskListInstance.startSpinner(data);
+    } else {
+      this.taskListInstance.stopSpinner();
     }
+  }
 }
 
 module.exports = class TaskList {
-    constructor(tasks, options = {}) {
-        this._tasks = tasks;
-        this._options = options;
-        this._index = 0;
-        this._tasksLength = tasks.length;
-        this._context = Object.create(null);
-        this._status = 'ready';
-        this._promise = new Promise((resolve, reject) => {
-            this._resolve = resolve;
-            this._reject = reject;
-        });
-    }
+  constructor(tasks, options = {}) {
+    this._tasks = tasks;
+    this._options = options;
+    this._index = 0;
+    this._tasksLength = tasks.length;
+    this._context = Object.create(null);
+    this._status = 'ready';
+    this._promise = new Promise((resolve, reject) => {
+      this._resolve = resolve;
+      this._reject = reject;
+    });
+  }
 
-    getContext() {
-        return this._context
-    }
+  getContext() {
+    return this._context
+  }
 }
--- packages/rom-cli-init/run.js
+++ packages/rom-cli-init/run.js
@@ -1,3 +1,14 @@
+const TaskList = require('./TaskList');
+
 module.exports = (template, appName, options = {}) => {
-    console.log(template, appName, options, 111)
+  console.log(template, appName, options, 111)
+
+  const tasks = []
+
+  const taskList = new TaskList(tasks);
+
+  taskList
+    .run().then(() => {
+
+    })
 }
